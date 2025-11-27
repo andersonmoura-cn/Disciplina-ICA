e75719f8e0af5ef618d9de90083fc8e64ca2980f
Anderson <andersonmoura.cn@gmail.com>
2025-11-26 23:30:37 -0300

    Eliminação da função k_fold duplicada

--- src/hw2/biblioteca.py
+++ src/hw2/biblioteca.py
@@ -16,7 +16,7 @@ from sklearn.preprocessing import StandardScaler
 
 np.random.seed(42)
 
-# mudanca do chat:
+# mudanca do chatgpt:
 def img_save(file_name):
     # Caminho base: projeto (DISCIPLINA-ICA)
     base = Path(__file__).resolve().parent.parent.parent  
@@ -42,6 +42,7 @@ def gerar_dados(n = 100, sigma = 0, grau = 5):
     y_teste = 3 + 5 * x_teste[:, 0] + ruido
     return X, y_teste
 
+
 def OLS_beta(x, y, lambida = 0):
     x = np.array(x)
     y = np.array(y)
@@ -65,51 +66,8 @@ def OLS_predict(X, Beta):
     y_pred = X_pred @ Beta
     return y_pred
 
-# x = [[1],[2],[3]]
-
-# y = [[8],[10],[22]]
-
-# Beta, X = OLS_beta(x, y)
-# y_p = OLS_predict(X, Beta)
-# print(y_p)
-
-
-def k_fold(x,y,k):
-    x = np.array(x)
-    y = np.array(y)
-
-    indices = np.arange(len(x))
-    np.random.shuffle(indices)
-
-    X_emb = x[indices]
-    Y_emb = y[indices]
-
-    folds_indices = np.array_split(indices, k)
-
-    folds = []
-    for i in range(k):
-        indices_teste = folds_indices[i]
-
-        indices_treino = np.concatenate([folds_indices[j] for j in range(k) if j != i])
-
-        X_treino, Y_treino = X_emb[indices_treino], Y_emb[indices_treino]
-        X_teste, Y_teste = X_emb[indices_teste], Y_emb[indices_teste]
-
-        folds.append([X_treino, Y_treino,X_teste, Y_teste])
-    
-    return folds
-
-
 # x = [1,2,3,4,5,6,7,8,9,10]
 # y = [3,4,5,2,3,7,7,8,9,10]
-
-# lepo = k_fold(x, y, 5)
-# for i in lepo:
-#     print(i[0], i[1], i[2], i[3])
-
-    
-    
-
 
 # Beta, X = OLS_beta(x, y)
 # y_p = OLS_predict(X, Beta)
